1b6b581cd79c64df6ccdbf99d52e2a93718a25ab
Alexandr Khromov <4-09@bk.ru>
2021-05-11 08:30:24 +0700

    added create and edit for note

--- app/src/main/java/com/example/androidjavageekbrainsnotes/presentation/adapter/NoteListAdapter.java
+++ app/src/main/java/com/example/androidjavageekbrainsnotes/presentation/adapter/NoteListAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidjavageekbrainsnotes.R;
@@ -19,16 +21,25 @@ import java.util.Locale;
 
 public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteListViewHolder> {
     private final List<Note> list = new ArrayList<>();
+    private final Fragment fragment;
+    private int longClickedPosition = -1;
 
-    public NoteListAdapter(OnNoteClicked clickListener) {
+    public NoteListAdapter(Fragment fragment, OnNoteClicked clickListener) {
+        this.fragment = fragment;
         this.clickListener = clickListener;
     }
 
     private final OnNoteClicked clickListener;
 
-    public void addData(List<Note> toAdd) {
+    public void setData(List<Note> toAdd) {
+        NotesDiffUtilCallback callback = new NotesDiffUtilCallback(list, toAdd);
+
+        DiffUtil.DiffResult result = DiffUtil.calculateDiff(callback);
+
         list.clear();
         list.addAll(toAdd);
+
+        result.dispatchUpdatesTo(this);
     }
 
     @NonNull
@@ -42,6 +53,14 @@ public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteLi
     public void onBindViewHolder(@NonNull NoteListViewHolder holder, int position) {
         Note note = list.get(position);
         holder.bind(note);
+    }
+
+    public int getLongClickedPosition() {
+        return longClickedPosition;
+    }
+
+    public Note getItemAt(int longClickedPosition) {
+        return list.get(longClickedPosition);
     }
 
     @Override
@@ -62,10 +81,18 @@ public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteLi
         public NoteListViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            fragment.registerForContextMenu(itemView);
+
             itemView.setOnClickListener(v -> {
                 if (getClickListener() != null) {
                     getClickListener().onNoteClicked(list.get(getAdapterPosition()));
                 }
+            });
+
+            itemView.setOnLongClickListener(v -> {
+                itemView.showContextMenu();
+                longClickedPosition = getAdapterPosition();
+                return true;
             });
 
             title = itemView.findViewById(R.id.title_note);
@@ -74,13 +101,51 @@ public class NoteListAdapter extends RecyclerView.Adapter<NoteListAdapter.NoteLi
         }
 
         public void bind(Note note) {
-            title.setText(note.getTitleRes());
-            text.setText(note.getTextRes());
+            title.setText(note.getTitle());
+            text.setText(note.getText());
             date.setText(sdf.format(note.getDate()));
         }
     }
 
     public interface OnNoteClicked {
         void onNoteClicked(Note note);
+    }
+
+    public static class NotesDiffUtilCallback extends DiffUtil.Callback {
+        private final List<Note> oldList;
+        private final List<Note> newList;
+
+        public NotesDiffUtilCallback(List<Note> oldList, List<Note> newList) {
+            this.oldList = oldList;
+            this.newList = newList;
+        }
+
+        @Override
+        public int getOldListSize() {
+            return oldList.size();
+        }
+
+        @Override
+        public int getNewListSize() {
+            return newList.size();
+        }
+
+        @Override
+        public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+            return oldList.get(oldItemPosition).getId().equals(newList.get(newItemPosition).getId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+            return oldList.get(oldItemPosition).equals(newList.get(newItemPosition));
+        }
+
+        public List<Note> getOldList() {
+            return oldList;
+        }
+
+        public List<Note> getNewList() {
+            return newList;
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidjavageekbrainsnotes/presentation/view/NoteEditFragment.java
@@ -0,0 +1,127 @@
+package com.example.androidjavageekbrainsnotes.presentation.view;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.androidjavageekbrainsnotes.R;
+import com.example.androidjavageekbrainsnotes.domain.model.Note;
+import com.example.androidjavageekbrainsnotes.presentation.viewModel.NoteListViewModel;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+
+public class NoteEditFragment extends Fragment {
+    private MaterialButton buttonEditNote;
+    private TextInputEditText titleNote;
+    private MaterialTextView dateNote;
+    private TextInputEditText textNote;
+    private NoteListViewModel viewModel;
+    private NavController navController;
+    DatePickerDialog.OnDateSetListener mDateListener;
+
+    public NoteEditFragment() {
+        // Required empty public constructor
+    }
+
+    public static NoteEditFragment newInstance(String param1, String param2) {
+        NoteEditFragment fragment = new NoteEditFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note_edit, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        DateFormat sdf = new SimpleDateFormat("dd.MM.yyyy hh:mm", Locale.ENGLISH);
+        viewModel = new ViewModelProvider(requireActivity()).get(NoteListViewModel.class);
+        navController = Navigation.findNavController(view);
+        buttonEditNote = view.findViewById(R.id.note_edit__button_edit);
+        titleNote = view.findViewById(R.id.note_edit__title);
+        textNote = view.findViewById(R.id.note_edit__text);
+        dateNote = view.findViewById(R.id.note_edit__date);
+        titleNote.setText(Objects.requireNonNull(viewModel.getSelectedNote().getValue()).getTitle());
+        textNote.setText(Objects.requireNonNull(viewModel.getSelectedNote().getValue()).getText());
+        dateNote.setText(sdf.format(Objects.requireNonNull(viewModel.getSelectedNote().getValue()).getDate()));
+        mDateListener = (datePicker, year, month, day) -> {
+            Date newDate = viewModel.getSelectedNote().getValue().getDate();
+            Date tempDate = null;
+            try {
+                tempDate = sdf.parse(day + "." + (month + 1) + "." + year + " " + newDate.getHours() + ":" + newDate.getMinutes());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if (tempDate != null) {
+                dateNote.setText(sdf.format(tempDate));
+            }
+        };
+        dateNote.setOnClickListener(v -> {
+            Date newDate = null;
+            try {
+                newDate = sdf.parse(dateNote.getText().toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Calendar cal = Calendar.getInstance();
+            if (newDate != null) {
+                cal.setTime(newDate);
+            }
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+
+            DatePickerDialog dialog = new DatePickerDialog(
+                    getContext(),
+                    android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    mDateListener,
+                    year, month, day
+            );
+            dialog.show();
+        });
+        buttonEditNote.setOnClickListener(v -> {
+            String title = Objects.requireNonNull(titleNote.getText()).toString().trim();
+            String text = Objects.requireNonNull(textNote.getText()).toString().trim();
+            String date = Objects.requireNonNull(dateNote.getText()).toString().trim();
+
+            if (title.equals("") || text.equals("")) return;
+
+            Note currentNote = viewModel.getSelectedNote().getValue();
+            currentNote.setTitle(title);
+            currentNote.setText(text);
+            try {
+                currentNote.setDate(sdf.parse(date));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            viewModel.updateNote(currentNote);
+            navController.navigateUp();
+        });
+    }
+}
